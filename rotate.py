5735eed7b6f19247d548a21e6f1fb777b3684d62
Vedant <vm.WorkAddress@gmail.com>
2019-01-24 02:58:32 -0500

    updated rotate

--- rotate.py
+++ rotate.py
@@ -9,7 +9,7 @@ outfn = 'rotate.%s' % os.path.basename(inpfn)
 trailer = PdfReader(inpfn)
 pages = trailer.pages
 for i in range(0, len(pages), 2):
-    pages[i].Rotate  = 270
+    pages[i].Rotate  = 180
 
 outdata = PdfWriter(outfn)
 outdata.trailer = trailer
